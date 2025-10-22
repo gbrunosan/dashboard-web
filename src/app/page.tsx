64f4ceaf56a6ac90bb305c01a1714d5ad7282271
Gerson Bruno <gbrunosan159@gmail.com>
2025-10-22 01:50:07 -0400

    lucide, charts e outros comp do shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,52 @@
-import { Button } from "@/components/ui/Button"
-
+"use client";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Cell, Pie, PieChart } from "recharts";
+import chartData from "../../chartData.json";
 export default function Shadcn() {
+    const COLORS = [
+      "hsl(var(--success))",
+      "hsl(var(--grayScale-400))",
+    ]
+
+    const chartConfig = {
+    quantidade: {
+      label: "Quantidade",
+    },
+    ativo: {
+      label: "Ativo",
+      color: "primary",
+    },
+    inativo: {
+      label: "Inativo",
+      color: "grayScale-400",
+    },
+  }
+
   return (
     <div className="flex gap-2">
-      <Button>Default</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button size="sm">Small</Button>
-      <Button size="lg">Large</Button>
-      <Button variant="ghost">Ghost</Button>
-      <div>
-      </div>
+      <ChartContainer
+        config={chartConfig} className="max-h-[500px] w-full"
+      >
+        <PieChart>
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <Pie
+            dataKey="quantidade"
+            nameKey="status"
+            data={chartData.quebras.contatos_por_status}
+          >
+            {chartData.quebras.contatos_por_status.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ChartContainer>
     </div>
-  )
+  );
 }
